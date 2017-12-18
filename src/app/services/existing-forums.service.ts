e35d168d3b06f786f8f0cdaafc33ccd3bf6879f2
Ivica <inikolovbiz@gmail.com>
2017-12-18 12:39:00 +0100

    api base url policy implemented

--- src/app/services/existing-forums.service.ts
+++ src/app/services/existing-forums.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import { Http, Response } from '@angular/http';
-
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ExistingForumsService {
 
-  baseUrl = 'http://localhost:3000/autogf/v1';
+  private baseUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
   getExistingForums(): any {
 
-    const endpoint = '/sites/2/forums';
+    const endpoint = 'sites/2/forums';
 
     // TO DO add siteid param !!!!!!!!!!
     return this.http.get(this.baseUrl + endpoint);
